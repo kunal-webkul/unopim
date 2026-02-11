8416b552352d3bc4c11c5dd6dd0c48d3f35494d1
Kunal kumar <111128645+kunal-webkul@users.noreply.github.com>
2026-02-12 00:06:11 +0530

    Remove serial mode configuration from category tests

--- tests/e2e-pw/tests/01-catalog/category.spec.js
+++ tests/e2e-pw/tests/01-catalog/category.spec.js
@@ -1,5 +1,4 @@
 const { test, expect } = require('../../utils/fixtures');
-test.describe.configure({ mode: 'serial' });
 test.describe('UnoPim Category', () => {
   test('Create Categories with empty Code field', async ({ adminPage }) => {
     await adminPage.getByRole('link', { name: ' Catalog' }).click();
