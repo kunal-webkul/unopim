2c3e6ec7d4dae908b94eeff50dc9810be159ca75
Kunal kumar <111128645+kunal-webkul@users.noreply.github.com>
2026-02-12 00:10:04 +0530

    Remove serial mode configuration from role tests

--- tests/e2e-pw/tests/05-settings/role.spec.js
+++ tests/e2e-pw/tests/05-settings/role.spec.js
@@ -1,5 +1,4 @@
 const { test, expect } = require('../../utils/fixtures');
-test.describe.configure({ mode: 'serial' });
 test.describe('UnoPim Test cases(Administrator Role)', () => {
 test('Create role with empty permission field', async ({ adminPage }) => {
   await adminPage.getByRole('link', { name: ' Settings' }).click();
